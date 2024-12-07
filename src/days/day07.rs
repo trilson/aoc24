52f9b8a36ced198427cf5e0f10cb218a5fddfd8b
Thomas Wilson <tom@trilson.co.uk>
2024-12-07 13:06:15 +0000

    day07

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,12 +1,43 @@
-use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
-
-///////////////////////////////////////////////////////////////////////////////
+use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let lines: Vec<String> = lines_from_file("input/day07.txt");
+    let mut sol1: i64 = 0;
+    let mut sol2: i64 = 0;
+
+    for mut line in lines.iter().map(|l| l.split(':')) {
+        let target: i64 = line
+            .next()
+            .expect("Must have a target")
+            .parse()
+            .expect("Target must be a number");
+        let numbers: Vec<i64> = line
+            .next()
+            .expect("Must have components")
+            .split_whitespace()
+            .map(|el| el.parse().expect("Component must be a number"))
+            .collect();
+
+        if is_valid(numbers.get(0).unwrap(), &target, &numbers[1..], false) {
+            sol1 += target;
+        }
+        if is_valid(numbers.get(0).unwrap(), &target, &numbers[1..], true) {
+            sol2 += target;
+        }
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn is_valid(cur: &i64, goal: &i64, rem: &[i64], sol2: bool) -> bool {
+    if cur > goal {
+        return false;
+    }
+    if rem.is_empty() {
+        return cur == goal;
+    }
+    let concat = cur * 10_i64.pow(((rem[0] as f64).log10().floor() as u32) + 1) + rem[0];
+    return is_valid(&(cur * rem[0]), goal, &rem[1..], sol2)
+        || is_valid(&(cur + rem[0]), goal, &rem[1..], sol2)
+        || (sol2 && is_valid(&concat, goal, &rem[1..], sol2));
 }
