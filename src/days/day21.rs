a90411b478e4186032ce59d761ab48c430472f09
Thomas Wilson <tom@trilson.co.uk>
2024-12-22 20:21:43 +0000

    day21pt1

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,12 +1,119 @@
-use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::collections::{HashSet, VecDeque};
 
-///////////////////////////////////////////////////////////////////////////////
+use bimap::BiMap;
+use once_cell::sync::Lazy;
+
+use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let codes = lines_from_file("input/day21.txt");
+    let mut sol1 = 0;
+    for code in codes {
+        let num: i64 = code[0..3].parse().expect("Integer value not found");
+        sol1 += num * solve_code_pt1(code, "AAA".to_string()).expect("Must have solution");
+    }
 
-    (Solution::from(sol1), Solution::from(sol2))
+    (Solution::from(sol1), Solution::from("N/A"))
+}
+
+static DIR_PAD: Lazy<BiMap<char, (i64, i64)>> = Lazy::new(|| {
+    let mut map = BiMap::new();
+    map.insert('^', (0, 1));
+    map.insert('A', (0, 2));
+    map.insert('<', (1, 0));
+    map.insert('v', (1, 1));
+    map.insert('>', (1, 2));
+    map
+});
+
+static NUM_PAD: Lazy<BiMap<char, (i64, i64)>> = Lazy::new(|| {
+    let mut map = BiMap::new();
+    map.insert('7', (0, 0));
+    map.insert('8', (0, 1));
+    map.insert('9', (0, 2));
+    map.insert('4', (1, 0));
+    map.insert('5', (1, 1));
+    map.insert('6', (1, 2));
+    map.insert('1', (2, 0));
+    map.insert('2', (2, 1));
+    map.insert('3', (2, 2));
+    map.insert('0', (3, 1));
+    map.insert('A', (3, 2));
+    map
+});
+
+fn solve_code_pt1(code: String, initial_state: String) -> Option<i64> {
+    let mut queue: VecDeque<(i64, Vec<char>, Vec<char>)> =
+        VecDeque::from([(0, code.chars().collect(), initial_state.chars().collect())]);
+
+    let mut visited = HashSet::new();
+
+    while let Some((presses, code_left, state)) = queue.pop_front() {
+        if code_left.is_empty() {
+            return Some(presses);
+        }
+        let visiting = (code_left.clone(), state.clone());
+        if visited.contains(&visiting) {
+            continue;
+        }
+        visited.insert(visiting);
+
+        for dir in ['^', '<', '>', 'v'] {
+            if let Some(moved) = find_next(dir, state[0], false) {
+                let mut moved_state = state.clone();
+                moved_state[0] = moved;
+                queue.push_back((1 + presses, code_left.clone(), moved_state));
+            }
+        }
+
+        let (a_state, a_code) = process_a_press(state, code_left);
+        queue.push_back((1 + presses, a_code, a_state));
+    }
+    None
+}
+
+fn process_a_press(mut state: Vec<char>, mut code: Vec<char>) -> (Vec<char>, Vec<char>) {
+    let (index, _ch) = state
+        .iter()
+        .enumerate()
+        .find(|(_, &ch)| ch != 'A')
+        .or_else(|| Some((state.len() - 1, &'A')))
+        .expect("Must have a target");
+
+    if index == state.len() - 1 {
+        let num_to_pop = state[index];
+        if num_to_pop == code[0] {
+            code.remove(0);
+        }
+    } else if index == state.len() - 2 {
+        if let Some(next_number) = find_next(state[index], state[index + 1], true) {
+            state.pop();
+            state.push(next_number);
+        }
+    } else {
+        if let Some(next_dir) = find_next(state[index], state[index + 1], false) {
+            state[index + 1] = next_dir;
+        }
+    }
+    (state, code)
+}
+
+fn find_next(direction: char, current: char, num_pad: bool) -> Option<char> {
+    let pad = if num_pad { &NUM_PAD } else { &DIR_PAD };
+    let current_pos: (i64, i64) = *pad
+        .get_by_left(&current)
+        .expect("Current location must exist");
+    let new_location = match direction {
+        '^' => (current_pos.0 - 1, current_pos.1),
+        '>' => (current_pos.0, current_pos.1 + 1),
+        'v' => (current_pos.0 + 1, current_pos.1),
+        '<' => (current_pos.0, current_pos.1 - 1),
+        _ => panic!("Shouldn't get here"),
+    };
+    pad.get_by_right(&new_location).copied()
+}
+
+#[test]
+fn s() {
+    solve();
 }
--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,12 +1,45 @@
-use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::{collections::HashMap, iter::successors};
 
-///////////////////////////////////////////////////////////////////////////////
+use crate::{utils::files::lines_from_file, Solution, SolutionPair};
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let sequences: Vec<i64> = lines_from_file("input/day22.txt")
+        .iter()
+        .filter_map(|c| c.parse().ok())
+        .collect();
+
+    let mut sol1: i64 = 0;
+    let mut optimal_bananas = HashMap::new();
+    for sequence in sequences {
+        let secrets: Vec<i64> = successors(Some(sequence), |cur| Some(next_sequence(*cur)))
+            .take(2001)
+            .collect();
+        sol1 += secrets.last().expect("Must have a 2000th sequence");
+
+        let bananas: Vec<i64> = secrets.iter().map(|x| x % 10).collect();
+        let deltas: Vec<i64> = bananas.windows(2).map(|x| x[1] - x[0]).collect();
+
+        let mut price_changes = HashMap::new();
+        for (id, window) in deltas.windows(4).enumerate() {
+            let window_vector = window.to_vec();
+            if !price_changes.contains_key(&window_vector) {
+                price_changes.insert(window_vector, bananas[id + 4]);
+            }
+        }
+        for (key, val) in price_changes {
+            *optimal_bananas.entry(key).or_insert(0_i64) += val;
+        }
+    }
+    let sol2: i64 = *optimal_bananas
+        .values()
+        .max()
+        .expect("Must have optimal number of bananas to sell");
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn next_sequence(sequence: i64) -> i64 {
+    let mut next = (sequence ^ (sequence << 6)) & 16777215;
+    next = (next ^ (next >> 5)) & 16777215;
+    (next ^ (next << 11)) & 16777215
 }
